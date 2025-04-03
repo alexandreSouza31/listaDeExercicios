38a669262aaf769e243d0f32a9d008517e986004
alexandreSouza31 <alexandre31_m@hotmail.com>
2025-04-03 20:46:28 -0300

    solução para exercício 04

---
+++ listaDeExercicio.Exercicio04/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace listaDeExercicio.Exercicio04
+{
+    internal class Program
+    {
+        //Crie um programa para converter a temperatura da escala Celsius para a escala Fahrenheit
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a temperatura em Celsius: ");
+            double celsius = Convert.ToDouble(Console.ReadLine());
+
+            double resultado = ((celsius * 9) / 5) + 32;
+
+            Console.WriteLine($" O consumo de combustível por quilômetro percorrido é: {resultado}");
+            Console.ReadLine();
+        }
+    }
+}
