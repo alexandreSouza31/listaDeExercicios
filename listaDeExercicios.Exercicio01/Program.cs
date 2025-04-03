ede320d67a0b5697c7d484967bdd7e8c4139b1f3
alexandreSouza31 <alexandre31_m@hotmail.com>
2025-04-03 19:51:51 -0300

    Add project files.

---
+++ listaDeExercicios.Exercicio01/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace listaDeExercicios.Exercicio01
+{
+    internal class Program
+    {
+        //crie um app para calcular o volume de uma caixa retangular
+        //volume=comprimento*largura*altura
+        static void Main(string[] args)
+        {
+            #region input de dados
+            Console.WriteLine("Digite o comprimento da caixa:");
+            decimal comprimento=Convert.ToDecimal(Console.ReadLine()); 
+
+            Console.WriteLine("Digite a largura da caixa:");
+            decimal largura=Convert.ToDecimal(Console.ReadLine()); 
+            
+            Console.WriteLine("Digite a altura da caixa:");
+            decimal altura=Convert.ToDecimal(Console.ReadLine());
+            #endregion
+
+            #region processamento de dados
+            decimal resultado =comprimento * largura * altura;
+            #endregion
+
+            #region output de dados
+            Console.WriteLine($"O volume da caixa é {resultado}");
+            Console.ReadLine();
+            #endregion
+        }
+    }
+}
---
+++ listaDeExercicios.Exercicio02/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace listaDeExercicios.Exercicio02
+{
+    internal class Program
+    {
+        //Crie um programa para calcular o volume de um Cilindro
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+        }
+    }
+}
