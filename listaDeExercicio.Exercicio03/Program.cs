555a3b4717db0036396be167be1f7f029b35ebba
alexandreSouza31 <alexandre31_m@hotmail.com>
2025-04-03 20:23:16 -0300

    criará a solução para o execício 3

---
+++ listaDeExercicio.Exercicio03/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace listaDeExercicio.Exercicio03
+{
+    internal class Program
+    {
+        //Crie um programa que calcule o consumo de combustível por quilômetro percorrido em uma viagem.O
+        //programa deve solicitar ao usuário:
+        //● A quilometragem inicial do veículo no início da viagem.
+        //● A quilometragem final ao término da viagem.
+        //● A quantidade de combustível consumida durante a viagem(em litros).
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o km inicial: ");
+            double kmInicial = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o km final: ");
+            double kmFinal = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite a quantidade de cobustível: ");
+            double qtdCombustivel = Convert.ToDouble(Console.ReadLine());
+
+            double resultado = Math.Abs((kmInicial - kmFinal) / qtdCombustivel);
+
+            Console.WriteLine($" O consumo de combustível por quilômetro percorrido é: {resultado}");
+            Console.ReadLine();
+        }
+    }
+}
