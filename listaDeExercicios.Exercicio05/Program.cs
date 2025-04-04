ec8db7a75892b398d2aab052e56e5720f247f4ab
alexandreSouza31 <alexandre31_m@hotmail.com>
2025-04-03 21:02:07 -0300

    criará a solução para o exercício 5

---
+++ listaDeExercicios.Exercicio05/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace listaDeExercicios.Exercicio05
+{
+    internal class Program
+    {
+        //5.Crie um programa para calcular o salário total de um vendedor.Deverá ser informado o salário base e o total de
+//vendas.A comissão é calculada com um percentual(informado pelo usuário) sobre o total de vendas.
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o salário base: ");
+            double salarioBase = Convert.ToDouble(Console.ReadLine());
+            
+            Console.Write("Digite o total de vendas: ");
+            double totalVendas = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o percentual de comissão: ");
+            double comissao = Convert.ToDouble(Console.ReadLine());
+
+            double resultado = salarioBase + ((totalVendas * comissao) / 100);
+
+            Console.WriteLine($"O salário total é {resultado}");
+            Console.ReadLine();
+        }
+    }
+}
+
