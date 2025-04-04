01bd2461bf76ae18b99ce21f1ccc342b2931ce5f
alexandreSouza31 <alexandre31_m@hotmail.com>
2025-04-04 11:44:23 -0300

    criará a solução para o exercício 8

---
+++ listaDeExercicios.Exercicio08/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace listaDeExercicios.Exercicio08
+{
+    //8. Faça um algoritmo que leia os valores A, B, C e imprima na tela se a soma de A + B é menor que C.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um valor para A: ");
+            double valorDeA = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite um valor para B: ");
+            double valorDeB = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite um valor para C: ");
+            double valorDeC = Convert.ToDouble(Console.ReadLine());
+
+            if(valorDeA + valorDeB < valorDeC)
+            {
+            Console.WriteLine($"A soma de A({valorDeA}) + B({valorDeB}) é menor que C ({valorDeC})");
+            Console.ReadLine();
+
+            }
+
+        }
+    }
+}
